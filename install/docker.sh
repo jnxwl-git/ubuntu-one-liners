62dba0e84ad6e0c96eca7462159025d84a7d50f7
John <107912641+jnxwl-git@users.noreply.github.com>
2025-06-06 22:24:07 +0300

    Delete install directory

--- install/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-
-sudo usermod -aG docker "$USER"
-
-echo "Docker успешно установлен. выполните 'newgrp docker'"
